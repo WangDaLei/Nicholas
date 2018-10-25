81dc0d3f0afc44f7e6caae7799aeff7a48e6c17d
WangLei <wangcppclei@gmail.com>
2018-10-25 23:00:30 +0800

    add unary line regression

--- quantitative_investment/controllers.py
+++ quantitative_investment/controllers.py
@@ -18,3 +18,20 @@ def get_standard_deviation(x):
 
 	re = (sum_all/(len(x)-1))**0.5
 	return round(re, 2)
+
+def get_unarg_parameter(x, y):
+	avg_x = sum(x)/len(x)
+	avg_y = sum(y)/len(y)
+
+	sum_xy = 0.0
+	for i in range(len(x)):
+		sum_xy += (x[i]-avg_x)*(y[i]-avg_y)
+
+	sum_x = 0.0
+	for i in range(len(x)):
+		sum_x += (x[i]-avg_x)**2
+
+	b1 = sum_xy / sum_x
+	b0 = avg_y - b1 * avg_x
+
+	return round(b1, 4), round(b0, 4)
