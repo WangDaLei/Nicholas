256d968e77a9a99c4b4d018efd90788b2b21bb54
WangLei <wangcppclei@gmail.com>
2018-10-25 16:24:33 +0800

    add correlation cooeff

--- quantitative_investment/controllers.py
+++ quantitative_investment/controllers.py
@@ -1,2 +1,20 @@
 '''
 '''
+
+def get_covariance(x, y):
+	avg_x = sum(x)/len(x)
+	avg_y = sum(y)/len(y)
+
+	sum_all = 0.0
+	for i in range(len(x)):
+		sum_all += (x[i]-avg_x)*(y[i]-avg_y)
+	return round(sum_all/(len(x)-1), 2)
+
+def get_standard_deviation(x):
+	avg_x = sum(x)/len(x)
+	sum_all = 0.0
+	for i in range(len(x)):
+		sum_all += (x[i]-avg_x)**2
+
+	re = (sum_all/(len(x)-1))**0.5
+	return round(re, 2)
--- quantitative_investment/index_correlation_coefficient.py
+++ quantitative_investment/index_correlation_coefficient.py
@@ -2,28 +2,11 @@
 import math
 from datetime import date
 from stock.models import IndexRecord
-
-def get_covariance(x, y):
-	avg_x = sum(x)/len(x)
-	avg_y = sum(y)/len(y)
-
-	sum_all = 0.0
-	for i in range(len(x)):
-		sum_all += (x[i]-avg_x)*(y[i]-avg_y)
-	return round(sum_all/len(x), 2)
-
-def get_standard_deviation(x):
-	avg_x = sum(x)/len(x)
-	sum_all = 0.0
-	for i in range(len(x)):
-		sum_all += (x[i]-avg_x)**2
-
-	re = (sum_all/len(x))**0.5
-	return round(re, 2)
-
+from .controllers import get_covariance, get_standard_deviation
 
 def get_correlation_coefficient(name='上证指数',start_date='2010-01-01', end_date=date.today()):
 	'''
+	    计算 指数变化的百分比 和 交易量的相关系数
 		交易量使用亿作为单位，涨跌幅采用采用去除百分号的小数
 	'''
 	records = IndexRecord.objects.filter(name=name, date__range=[start_date, end_date]).order_by('date')
@@ -34,12 +17,56 @@ def get_correlation_coefficient(name='上证指数',start_date='2010-01-01', end
 			continue
 		else:
 			trade_amount_list.append(round(record.trade_amount, 2))
-			rate = round((record.close_index - records[i-1].close_index)/records[i-1].close_index*100, 2)
+			rate = abs(round((record.close_index - records[i-1].close_index)/records[i-1].close_index*100, 2))
 			change_percent_list.append(rate)
 	rate = get_covariance(trade_amount_list, change_percent_list)/(get_standard_deviation(trade_amount_list)*\
 			get_standard_deviation(change_percent_list))
 	return rate
 
-if __name__ == "__main__":
-	rate = get_correlation_coefficient()
-	print(rate)
+def get_correlation_coefficient_of_index(start_date='2010-01-01', end_date=date.today()):
+	'''
+		计算 两个指数之间的相关系数
+	'''
+	shang_records = IndexRecord.objects.filter(name='上证指数', date__range=[start_date, end_date]).order_by('date')
+	shen_records = IndexRecord.objects.filter(name='深证指数', date__range=[start_date, end_date]).order_by('date')
+	
+	shang_list = []
+	shen_list = []
+	
+	for i, record in enumerate(shang_records):
+		shang_list.append(round(record.close_index, 2))
+
+	for i, record in enumerate(shen_records):
+		shen_list.append(round(record.close_index, 2))
+
+	rate = get_covariance(shang_list, shen_list)/(get_standard_deviation(shang_list)*\
+			get_standard_deviation(shen_list))
+	return rate
+
+def get_correlation_coefficient_of_index_percent(start_date='2010-01-01', end_date=date.today()):
+	'''
+		计算 两个指数变化率的相关系数
+	'''
+	shang_records = IndexRecord.objects.filter(name='上证指数', date__range=[start_date, end_date]).order_by('date')
+	shen_records = IndexRecord.objects.filter(name='深证指数', date__range=[start_date, end_date]).order_by('date')
+	
+	shang_list = []
+	shen_list = []
+	
+	for i, record in enumerate(shang_records):
+		if not i:
+			continue
+		else:
+			rate = round((record.close_index - shang_records[i-1].close_index)/shang_records[i-1].close_index*100, 2)
+			shang_list.append(round(rate, 2))
+
+	for i, record in enumerate(shen_records):
+		if not i:
+			continue
+		else:
+			rate = round((record.close_index - shen_records[i-1].close_index)/shen_records[i-1].close_index*100, 2)
+			shen_list.append(round(rate, 2))
+
+	rate = get_covariance(shang_list, shen_list)/(get_standard_deviation(shang_list)*\
+			get_standard_deviation(shen_list))
+	return rate
