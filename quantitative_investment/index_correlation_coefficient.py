69a984f68ec5414e4852493e9b432997760cfd15
WangDaLei <wangcppclei@gmail.com>
2019-01-08 14:28:44 +0800

    fix format

--- quantitative_investment/index_correlation_coefficient.py
+++ quantitative_investment/index_correlation_coefficient.py
@@ -1,15 +1,18 @@
 
-import math
 from datetime import date
 from stock.models import IndexRecord
 from .controllers import get_covariance, get_standard_deviation
 
-def get_correlation_coefficient(name='上证指数',start_date='2010-01-01', end_date=date.today()):
-    '''
+
+def get_correlation_coefficient(name='上证指数', start_date='2010-01-01', end_date=date.today()):
+    """
+    Caculate.
+
         计算 指数变化的百分比 和 交易量的相关系数
         交易量使用亿作为单位，涨跌幅采用采用去除百分号的小数
-    '''
-    records = IndexRecord.objects.filter(name=name, date__range=[start_date, end_date]).order_by('date')
+    """
+    records = IndexRecord.objects.filter(
+        name=name, date__range=[start_date, end_date]).order_by('date')
     trade_amount_list = []
     change_percent_list = []
     for i, record in enumerate(records):
@@ -17,56 +20,71 @@ def get_correlation_coefficient(name='上证指数',start_date='2010-01-01', end
             continue
         else:
             trade_amount_list.append(round(record.trade_amount, 2))
-            rate = abs(round((record.close_index - records[i-1].close_index)/records[i-1].close_index*100, 2))
+            rate = abs(round((record.close_index - records[i - 1].close_index) / records[i - 1]
+                       .close_index * 100, 2))
             change_percent_list.append(rate)
-    rate = get_covariance(trade_amount_list, change_percent_list)/(get_standard_deviation(trade_amount_list)*\
+    rate = get_covariance(trade_amount_list, change_percent_list) /\
+        (get_standard_deviation(trade_amount_list) *
             get_standard_deviation(change_percent_list))
     return rate
 
+
 def get_correlation_coefficient_of_index(start_date='2010-01-01', end_date=date.today()):
-    '''
+    """
+    Caculate.
+
         计算 两个指数之间的相关系数
-    '''
-    shang_records = IndexRecord.objects.filter(name='上证指数', date__range=[start_date, end_date]).order_by('date')
-    shen_records = IndexRecord.objects.filter(name='深证指数', date__range=[start_date, end_date]).order_by('date')
-    
+    """
+    shang_records = IndexRecord.objects.filter(
+        name='上证指数', date__range=[start_date, end_date]).order_by('date')
+    shen_records = IndexRecord.objects.filter(
+        name='深证指数', date__range=[start_date, end_date]).order_by('date')
+
     shang_list = []
     shen_list = []
-    
+
     for i, record in enumerate(shang_records):
         shang_list.append(round(record.close_index, 2))
 
     for i, record in enumerate(shen_records):
         shen_list.append(round(record.close_index, 2))
 
-    rate = get_covariance(shang_list, shen_list)/(get_standard_deviation(shang_list)*\
+    rate = get_covariance(shang_list, shen_list) /\
+        (get_standard_deviation(shang_list) *
             get_standard_deviation(shen_list))
     return rate
 
+
 def get_correlation_coefficient_of_index_percent(start_date='2010-01-01', end_date=date.today()):
-    '''
+    """
+    Calculate.
+
         计算 两个指数变化率的相关系数
-    '''
-    shang_records = IndexRecord.objects.filter(name='上证指数', date__range=[start_date, end_date]).order_by('date')
-    shen_records = IndexRecord.objects.filter(name='深证指数', date__range=[start_date, end_date]).order_by('date')
-    
+    """
+    shang_records = IndexRecord.objects.filter(
+        name='上证指数', date__range=[start_date, end_date]).order_by('date')
+    shen_records = IndexRecord.objects.filter(
+        name='深证指数', date__range=[start_date, end_date]).order_by('date')
+
     shang_list = []
     shen_list = []
-    
+
     for i, record in enumerate(shang_records):
         if not i:
             continue
         else:
-            rate = round((record.close_index - shang_records[i-1].close_index)/shang_records[i-1].close_index*100, 2)
+            rate = round((record.close_index - shang_records[i - 1].close_index) /
+                         shang_records[i - 1].close_index * 100, 2)
             shang_list.append(round(rate, 2))
 
     for i, record in enumerate(shen_records):
         if not i:
             continue
         else:
-            rate = round((record.close_index - shen_records[i-1].close_index)/shen_records[i-1].close_index*100, 2)
+            rate = round((record.close_index - shen_records[i - 1].close_index) /
+                         shen_records[i - 1].close_index * 100, 2)
             shen_list.append(round(rate, 2))
 
-    rate = get_covariance(shang_list, shen_list)/(get_standard_deviation(shang_list)*\
-            get_standard_deviation(shen_list))
+    rate = get_covariance(shang_list, shen_list) / (get_standard_deviation(shang_list) *
+                                                    get_standard_deviation(shen_list))
     return rate
--- quantitative_investment/unary_linear_regression.py
+++ quantitative_investment/unary_linear_regression.py
@@ -3,67 +3,84 @@ from datetime import date
 from stock.models import IndexRecord
 from .controllers import get_unarg_parameter
 
-def get_unary_linear_regression(name='上证指数',start_date='2010-01-01', end_date=date.today()):
-	'''
-	    计算 指数变化的百分比 和 交易量的相关系数
-		交易量使用亿作为单位，涨跌幅采用采用去除百分号的小数
-	'''
-	records = IndexRecord.objects.filter(name=name, date__range=[start_date, end_date]).order_by('date')
-	trade_amount_list = []
-	change_percent_list = []
-	for i, record in enumerate(records):
-		if not i:
-			continue
-		else:
-			trade_amount_list.append(round(record.trade_amount, 2))
-			rate = abs(round((record.close_index - records[i-1].close_index)/records[i-1].close_index*100, 2))
-			change_percent_list.append(rate)
 
-	b1, b0 = get_unarg_parameter(trade_amount_list, change_percent_list)
-	return b1, b0
+def get_unary_linear_regression(name='上证指数', start_date='2010-01-01', end_date=date.today()):
+    """
+    Calculate.
+
+        计算 指数变化的百分比 和 交易量的相关系数
+        交易量使用亿作为单位，涨跌幅采用采用去除百分号的小数
+    """
+    records = IndexRecord.objects.filter(
+        name=name, date__range=[start_date, end_date]).order_by('date')
+    trade_amount_list = []
+    change_percent_list = []
+    for i, record in enumerate(records):
+        if not i:
+            continue
+        else:
+            trade_amount_list.append(round(record.trade_amount, 2))
+            rate = abs(round((record.close_index - records[i - 1].close_index) /
+                             records[i - 1].close_index * 100, 2))
+            change_percent_list.append(rate)
+
+    b1, b0 = get_unarg_parameter(trade_amount_list, change_percent_list)
+    return b1, b0
+
 
 def get_unary_linear_regression_of_index(start_date='2010-01-01', end_date=date.today()):
-	'''
-		计算 两个指数之间的一元回归
-	'''
-	shang_records = IndexRecord.objects.filter(name='上证指数', date__range=[start_date, end_date]).order_by('date')
-	shen_records = IndexRecord.objects.filter(name='深证指数', date__range=[start_date, end_date]).order_by('date')
-	
-	shang_list = []
-	shen_list = []
-	
-	for i, record in enumerate(shang_records):
-		shang_list.append(round(record.close_index, 2))
+    """
+    Calculate.
 
-	for i, record in enumerate(shen_records):
-		shen_list.append(round(record.close_index, 2))
+        计算 两个指数之间的一元回归
+    """
+    shang_records = IndexRecord.objects.filter(
+        name='上证指数', date__range=[start_date, end_date]).order_by('date')
+    shen_records = IndexRecord.objects.filter(
+        name='深证指数', date__range=[start_date, end_date]).order_by('date')
 
-	b1, b0 = get_unarg_parameter(shang_list, shen_list)
-	return b1, b0
+    shang_list = []
+    shen_list = []
+
+    for i, record in enumerate(shang_records):
+        shang_list.append(round(record.close_index, 2))
+
+    for i, record in enumerate(shen_records):
+        shen_list.append(round(record.close_index, 2))
+
+    b1, b0 = get_unarg_parameter(shang_list, shen_list)
+    return b1, b0
+
 
 def get_unary_linear_regression_of_index_percent(start_date='2010-01-01', end_date=date.today()):
-	'''
-		计算 两个指数变化率的一元回归
-	'''
-	shang_records = IndexRecord.objects.filter(name='上证指数', date__range=[start_date, end_date]).order_by('date')
-	shen_records = IndexRecord.objects.filter(name='深证指数', date__range=[start_date, end_date]).order_by('date')
-	
-	shang_list = []
-	shen_list = []
-	
-	for i, record in enumerate(shang_records):
-		if not i:
-			continue
-		else:
-			rate = round((record.close_index - shang_records[i-1].close_index)/shang_records[i-1].close_index*100, 2)
-			shang_list.append(round(rate, 2))
+    """
+    Calculate.
 
-	for i, record in enumerate(shen_records):
-		if not i:
-			continue
-		else:
-			rate = round((record.close_index - shen_records[i-1].close_index)/shen_records[i-1].close_index*100, 2)
-			shen_list.append(round(rate, 2))
+        计算 两个指数变化率的一元回归
+    """
+    shang_records = IndexRecord.objects.filter(
+        name='上证指数', date__range=[start_date, end_date]).order_by('date')
+    shen_records = IndexRecord.objects.filter(
+        name='深证指数', date__range=[start_date, end_date]).order_by('date')
 
-	b1, b0 = get_unarg_parameter(shang_list, shen_list)
-	return b1, b0
+    shang_list = []
+    shen_list = []
+
+    for i, record in enumerate(shang_records):
+        if not i:
+            continue
+        else:
+            rate = round((record.close_index - shang_records[i - 1].close_index) /
+                         shang_records[i - 1].close_index * 100, 2)
+            shang_list.append(round(rate, 2))
+
+    for i, record in enumerate(shen_records):
+        if not i:
+            continue
+        else:
+            rate = round((record.close_index - shen_records[i - 1].close_index) /
+                         shen_records[i - 1].close_index * 100, 2)
+            shen_list.append(round(rate, 2))
+
+    b1, b0 = get_unarg_parameter(shang_list, shen_list)
+    return b1, b0
